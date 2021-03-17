2c63d0175b1619ff1d77860602f09d8c688970e5
Zhicheng Liu <Zhicheng-Liu@users.noreply.github.com>
2021-03-17 21:24:09 +0000

    Feature/insert genotypes into redis (#3)
    * Add preparing genotypes file
    
    * Fix bug
    
    * Add script to insert genotypes into redis
    
    * Add further documentation
    
    * Add script for inserting tree leaf genotypes into redis
    
    * Add final documentation

---
+++ bin/prepare_sample_genotypes_file.py
@@ -0,0 +1,25 @@
+import argparse
+
+
+def _generate_sample_genotype_file(sample_mapping_csv, sample_genotypes_filepath):
+    isolate_to_tracking = {}
+    with open(sample_mapping_csv, "r") as f:
+        for line in f:
+            isolate, experiment, tracking = line.strip().split(',', maxsplit=2)
+            isolate_to_tracking[isolate.strip()] = tracking.strip()
+
+    with open(sample_genotypes_filepath, "r") as f:
+        for line in f:
+            isolate, genotypes = line.rstrip().split('\t', maxsplit=1)
+            if isolate not in isolate_to_tracking:
+                continue
+            print(f"{isolate_to_tracking[isolate]}\t{genotypes}")
+
+
+parser = argparse.ArgumentParser(description="Generate sample genotypes for uploading to redis")
+parser.add_argument('sample_mapping_csv', type=str, help='Path to the sample mapping csv')
+parser.add_argument('sample_genotypes_filepath', type=str, help='Path to the sample genotypes file')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    _generate_sample_genotype_file(args.sample_mapping_csv, args.sample_genotypes_filepath)
