eb532a78370686a79001e4ee54b1260cd99c109a
Zhicheng Liu <Zhicheng-Liu@users.noreply.github.com>
2021-05-11 14:40:25 +0100

    Upload new dataset and merge with existing one (#4)
    * Add upload new bigsi index and merge with an existing one.
    
    * Only generate distance data csv for included samples
    
    * Only generate distance data csv for included samples
    
    * fix

--- bin/generate_sample_nearest_neighbours_csv.py
+++ bin/generate_sample_nearest_neighbours_csv.py
@@ -1,7 +1,13 @@
 import argparse
 
 
-def _generate_requests(sample_mapping_csv, sample_nearest_neighbours_filepath, distance_threshold):
+def _generate_requests(included_samples_file, sample_mapping_csv, sample_nearest_neighbours_filepath, distance_threshold):
+    included_samples = []
+    with open(included_samples_file, "r") as f:
+        for line in f:
+            isolate, _ = line.strip().split(',', maxsplit=1)
+            included_samples.append(isolate)
+
     isolate_to_tracking = {}
     with open(sample_mapping_csv, "r") as f:
         for line in f:
@@ -11,15 +17,17 @@ def _generate_requests(sample_mapping_csv, sample_nearest_neighbours_filepath, d
     with open(sample_nearest_neighbours_filepath, "r") as f:
         first_line = next(f)
         target_list = list(map(lambda x: x.strip(), first_line.rstrip().split('\t')))
-        line_num = 1
         for line in f:
-            line_num += 1
             distances = line.rstrip().split('\t')
             source_sample = distances[0].strip()
+            if source_sample not in included_samples:
+                continue
             if source_sample not in isolate_to_tracking:
                 continue
-            for i in range(line_num, len(distances)):
+            for i in range(1, len(distances)):
                 target_sample = target_list[i]
+                if target_sample == source_sample:
+                    continue
                 if target_sample not in isolate_to_tracking:
                     continue
                 if distance_threshold >= int(distances[i]):
@@ -27,10 +35,11 @@ def _generate_requests(sample_mapping_csv, sample_nearest_neighbours_filepath, d
 
 
 parser = argparse.ArgumentParser(description="Generate CSV for sample nearest neighbours upload")
+parser.add_argument('included_samples_file', type=str, help='Path to the included samples file, you can use the same file as mapping file')
 parser.add_argument('sample_mapping_csv', type=str, help='Path to the sample mapping csv')
 parser.add_argument('sample_nearest_neighbours_filepath', type=str, help='Path to the sample nearest neighbours file')
 parser.add_argument('distance_threshold', type=int, help='The distance threshold for nearest neighbours')
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    _generate_requests(args.sample_mapping_csv, args.sample_nearest_neighbours_filepath, args.distance_threshold)
+    _generate_requests(args.included_samples_file, args.sample_mapping_csv, args.sample_nearest_neighbours_filepath, args.distance_threshold)
