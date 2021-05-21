690e93ba82a76e73958c2f4ba6e86c4c1579c026
Zhicheng Liu <Zhicheng-Liu@users.noreply.github.com>
2021-05-22 00:15:34 +0100

    Group zero distanced samples in minimum spanning tree (#6)
    * Renaming variables
    
    * Group zero distanced samples

--- bin/query_mst.py
+++ bin/query_mst.py
@@ -17,27 +17,85 @@ def _convert_key_to_bytes(key):
 
 def _query_for_mst(sample_id, berkeley_db_file):
     neighbours, distances = _query_db(berkeley_db_file, sample_id)
-    nodes = neighbours.split(',')
-    num_nodes = len(nodes)
-    input_matrix = csr_matrix(np.frombuffer(ast.literal_eval(distances), dtype=np.uint8).reshape(num_nodes, num_nodes))
+    samples = neighbours.split(',')
+    num_samples = len(samples)
+    input_matrix = csr_matrix(np.frombuffer(ast.literal_eval(distances), dtype=np.uint8).reshape(num_samples, num_samples))
     mst = minimum_spanning_tree(input_matrix)
-    tree = _extract_minimum_spanning_tree(nodes, mst)
-    print(json.dumps(tree))
+    tree = _extract_minimum_spanning_tree(samples, mst)
+    print(tree)
 
 
-def _extract_minimum_spanning_tree(nodes, mst):
-    relationships = []
-    num_of_nodes = len(nodes)
+def _extract_minimum_spanning_tree(samples, mst):
+    num_samples = len(samples)
     matrix = mst.toarray()
-    for row in range(num_of_nodes - 1):
-        for col in range(row + 1, num_of_nodes):
-            if matrix[row][col] > 0:
+
+    # first iteration looks at the 0-distance samples and group them
+    grouped_sets = []
+    for row in range(num_samples - 1):
+        for col in range(row + 1, num_samples):
+            if matrix[row][col] == 1:
+                new_set = True
+                for s in grouped_sets:
+                    if samples[row] in s:
+                        s.add(samples[col])
+                        new_set = False
+                        break
+                    elif samples[col] in s:
+                        s.add(samples[row])
+                        new_set = False
+                        break
+                if new_set:
+                    grouped_sets.append({samples[row], samples[col]})
+                matrix[row][col] = 0
+
+    # second iteration connects those grouped sets and other singletons
+    nodes = []
+    relationships = []
+    sample_to_node_id = {}
+    for index, grouped_samples in enumerate(grouped_sets):
+        nodes.append({
+            "id": index,
+            "samples": grouped_samples
+        })
+        for sample in grouped_samples:
+            sample_to_node_id[sample] = index
+    node_count = len(nodes)
+    for row in range(num_samples - 1):
+        for col in range(row + 1, num_samples):
+            if matrix[row][col] > 1:
+                start_index = 0
+                if samples[row] in sample_to_node_id:
+                    start_index = sample_to_node_id[samples[row]]
+                else:
+                    start_index = node_count
+                    node_count = node_count + 1
+                    sample_to_node_id[samples[row]] = start_index
+                    nodes.append({
+                        "id": start_index,
+                        "samples": [samples[row]]
+                    })
+
+                end_index = 0
+                if samples[col] in sample_to_node_id:
+                    end_index = sample_to_node_id[samples[col]]
+                else:
+                    end_index = node_count
+                    node_count = node_count + 1
+                    sample_to_node_id[samples[col]] = end_index
+                    nodes.append({
+                        "id": end_index,
+                        "samples": [samples[col]]
+                    })
+
                 relationships.append({
-                    "start": nodes[row],
-                    "end": nodes[col],
+                    "start": start_index,
+                    "end": end_index,
                     "distance": int(matrix[row][col] - 1)
                 })
-    return relationships
+    return {
+        "nodes": nodes,
+        "distance": relationships
+    }
 
 
 def _query_db(berkeley_db_file, sample_id):
